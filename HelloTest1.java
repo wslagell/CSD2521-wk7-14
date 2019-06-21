69f0d185088ef6591fdbe8fa8459bb0808c2864a
Albert Tokarsky <41795885+alto44@users.noreply.github.com>
2019-06-21 11:27:35 -0400

    Add files via upload

---
+++ HelloTest1.java
@@ -0,0 +1,14 @@
+/*
+ * Created on 8/3/18 by AJT
+ */
+package hello.test.pkg1;
+
+public class HelloTest1 {
+    /*
+     * Replace your comments here!!
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+        System.out.println("Test 3");
+        }
+    }
