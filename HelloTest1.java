4b3a242a2909736e76bb4c7b6adfa8f85e942303
wslagell <54343744+wslagell@users.noreply.github.com>
2019-10-04 22:40:56 -0400

    Update HelloTest1.java
    I added an update on line three and modified the comments on Line 9, formally line 8.

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,12 @@
 /*
  * Created on 8/3/18 by AJT
+ * Updated on 10-4-2019 WS
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * This is my first pull request.
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
